a56e04f2ea554ded97813c148d026c87412257b6
Michael Benrubi <q100djmikeb@gmail.com>
2017-05-04 15:16:08 -0400

    implemented authorization

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,39 +1,31 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: [:show, :edit, :update, :destroy]
-
-  # GET /notes
-  # GET /notes.json
-  def index
-    @notes = Note.all
-  end
-
-  # GET /notes/1
-  # GET /notes/1.json
-  def show
-  end
-
-  # GET /notes/new
-  def new
-    @note = Note.new
-  end
-
-  # GET /notes/1/edit
-  def edit
-  end
+ before_action :set_note, only: [:show, :edit, :update, :destroy]
+ before_action :require_logged_in
+ # GET /notes
+ # GET /notes.json
+ def index
+ @notes = current_user.notes.all
+ end
+ # GET /notes/1
+ # GET /notes/1.json
+ def show
+ end
+ # GET /notes/new
+ def new
+ @note = current_user.notes.new
+ end
+ # GET /notes/1/edit
+ def edit
+ end
 
   # POST /notes
   # POST /notes.json
   def create
-    @note = Note.new(note_params)
+    @note = current_user.notes.new(note_params)
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+
     end
   end
 
@@ -64,11 +56,11 @@ class NotesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = Note.find(params[:id])
+      @note = current_user.notes.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def note_params
-      params.require(:note).permit(:user_id, :name, :body)
+      params.require(:note).permit(:name, :body)
     end
 end
